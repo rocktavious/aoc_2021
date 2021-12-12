1d91c981e85a795bc83a78854ba84325ea12c9ef
Kyle Rockman <rocktavious@icloud.com>
2021-12-12 05:22:48 -0600

    day 8 part 2 (unsolved)

--- day8/main.go
+++ day8/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -26,18 +27,14 @@ func readFile(path string) ([]string, error) {
 }
 
 func readInputData() {
-	lines, err := readFile("./input.dat")
+	lines, err := readFile("./sample.dat")
 	if err != nil {
 		panic(err)
 	}
-	for _, line := range lines {
+	values = make([][]string, len(lines))
+	for i, line := range lines {
 		parts := strings.Split(line, "|")
-		chunks := strings.Split(parts[1], " ")
-		for _, chunk := range chunks {
-			if len(chunk) >= 1 {
-				values = append(values, chunk)
-			}
-		}
+		values[i] = strings.Split(parts[1], " ")
 	}
 }
 
@@ -56,22 +53,72 @@ func checkError(err error) {
 ////////
 
 var (
-	values []string
+	values [][]string
+	lookup map[string]string = map[string]string{
+		"cagedb": "0",
+		//"ab":      "1",
+		"cdgba": "2",
+		//"cedba": "3",
+		//"eafb":    "4",
+		//"dcbef":  "5",
+		"bcgafe": "6",
+		//"dab":     "7",
+		//"acedgfb": "8",
+		"cefbgd": "9",
+	}
 )
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func sortedChars(s string) string {
+	charArray := sortRunes([]rune(s))
+	sort.Sort(charArray)
+	return string(charArray)
+}
 
 func calculateAnswer() {
 	count := 0
-	for _, chunk := range values {
-		switch len(chunk) {
-		case 2:
-			count += 1
-		case 3:
-			count += 1
-		case 4:
-			count += 1
-		case 7:
-			count += 1
+	sortedLookup := make(map[string]string, 10)
+	for key, value := range lookup {
+		sortedLookup[sortedChars(key)] = value
+	}
+	for _, line := range values {
+		representation := ""
+		for _, chunk := range line {
+			length := len(chunk)
+			if length >= 1 {
+				switch length {
+				case 2:
+					representation = fmt.Sprintf("%s%s", representation, "1")
+				case 3:
+					representation = fmt.Sprintf("%s%s", representation, "7")
+				case 4:
+					representation = fmt.Sprintf("%s%s", representation, "4")
+				case 7:
+					representation = fmt.Sprintf("%s%s", representation, "8")
+				default:
+					// Do Lookup
+					r := sortedLookup[sortedChars(chunk)]
+					if r == "" {
+						r = "#"
+					}
+					representation = fmt.Sprintf("%s%s", representation, r)
+				}
+			}
 		}
+		fmt.Printf("%s\n", representation)
 	}
 	fmt.Printf("%d\n", count)
 }
